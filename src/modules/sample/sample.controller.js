e2f456ad9f6b0edfc49071dfd0ed3ad15bb9f3bd
Kazuia <Kazuia@users.noreply.github.com>
2017-01-13 12:57:41 +0100

    adding animateCSS control tools

--- src/modules/sample/sample.controller.js
+++ src/modules/sample/sample.controller.js
@@ -11,6 +11,11 @@
         var vm = this;
         vm.title = 'Sample';
         vm.notify = notify;
+        vm.animate = animate;
+
+        vm.bounce = false;
+        vm.swing = false;
+        vm.flash = false;
 
         activate();
 
@@ -25,6 +30,18 @@
               icon: 'img/icon.ico'
             }
           );
+        }
+
+        function animate(type) {
+          vm.bounce = false;
+          vm.swing = false;
+          vm.flash = false;
+
+          console.log(type);
+
+          if (type === 'bounce') {vm.bounce = true;}
+          else if(type === 'flash') {vm.flash = true;}
+          else if(type === 'swing') {vm.swing = true;}
         }
     }
 })();
